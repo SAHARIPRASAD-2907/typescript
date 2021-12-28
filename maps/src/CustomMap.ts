ab5eda168465e6e7e544ef3fd2e75059a5b76a12
S A HARIPRASAD <myharicode@gmail.com>
2021-12-28 09:37:04 +0530

    adding marker for company and user

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,3 +1,6 @@
+import { User } from "./User";
+import { Company } from "./Company";
+
 export class CustomMap{
     private googleMap:google.maps.Map;
     constructor(divID:string){
@@ -9,5 +12,23 @@ export class CustomMap{
             }
         })
     }
-    
+    addUserMarker(user: User):void{
+        new google.maps.Marker({
+            map:this.googleMap,
+            position:{
+                lat:user.location.lat,
+                lng:user.location.lng
+            }
+        })
+    }
+    addCompanyMarker(company:Company):void{
+        new google.maps.Marker({
+            map:this.googleMap,
+            position:{
+                lat:company.location.lat,
+                lng:company.location.lng
+            }
+        })
+    }
+
 }
