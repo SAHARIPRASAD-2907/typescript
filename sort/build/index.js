8d47b62c06351f9d4dfd30ca8c758aceef8a1e66
S A HARIPRASAD <myharicode@gmail.com>
2022-01-18 13:49:08 +0530

    setting up typescript to run on comand line using concurrently

---
+++ sort/build/index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("hi there");
+var logSomething = function () {
+    console.log('something');
+};
+logSomething();
+// use tsc -w to watch continues change
---
+++ sort/src/index.ts
@@ -0,0 +1,9 @@
+console.log("hi there");
+
+const logSomething = ()=>{
+    console.log('something');
+}
+
+logSomething();
+
+// use tsc -w to watch continues change
